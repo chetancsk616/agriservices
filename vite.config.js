5446e9e0a29b6f687dfedf625ab27d21f50a94c6
kutikuppala chetan srinivas <kutikuppala.chetan2023@vitstudent.ac.in>
2025-07-19 22:18:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  // The 'base' property is removed to use the default '/'
 })
-
