7ef46dcdd1e69474d4d4e5925cf48d1fbd30d2d2
kutikuppala chetan srinivas <kutikuppala.chetan2023@vitstudent.ac.in>
2025-07-20 23:01:00 +0530

    Update FarmerLogin.jsx

--- src/FarmerLogin.jsx
+++ src/FarmerLogin.jsx
@@ -1,4 +1,4 @@
-import React, 'useState', useEffect, useContext } from 'react';
+import React, {'useState', useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './style.css';
 import supabase from '/src/supabaseClient';
