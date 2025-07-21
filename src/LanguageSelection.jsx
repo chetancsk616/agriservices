806735567855ed05b9ce30d8c8d973bdd89bca83
kutikuppala chetan srinivas <kutikuppala.chetan2023@vitstudent.ac.in>
2025-07-21 23:07:30 +0530

    Update LanguageSelection.jsx

--- src/LanguageSelection.jsx
+++ src/LanguageSelection.jsx
@@ -1,16 +1,27 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { LanguageContext } from './LanguageContext';
+import { LanguageContext } from './LanguageContext.jsx';
 import './style.css';
 
 const LanguageSelection = () => {
   const navigate = useNavigate();
   const { setLanguage } = useContext(LanguageContext);
 
-  // Simplified handler
   const handleLanguageClick = (lang) => {
     setLanguage(lang);
     navigate('/main/0');
+  };
+  
+  const handleSpeak = (event, text, langCode) => {
+    event.stopPropagation();
+
+    if ('speechSynthesis' in window) {
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = langCode;
+      window.speechSynthesis.speak(utterance);
+    } else {
+      alert('Sorry, your browser does not support text-to-speech.');
+    }
   };
 
   return (
@@ -24,22 +35,34 @@ const LanguageSelection = () => {
 
       <div className="select">
         <div className="lang" onClick={() => handleLanguageClick('en')}>
-          <div className="sound"></div> {/* Style is now in CSS */}
+          <div
+            className="sound"
+            onClick={(e) => handleSpeak(e, 'English', 'en-US')}
+          ></div>
           English
         </div>
 
         <div className="lang" onClick={() => handleLanguageClick('te')}>
-          <div className="sound"></div>
+          <div
+            className="sound"
+            onClick={(e) => handleSpeak(e, 'తెలుగు', 'te-IN')}
+          ></div>
           తెలుగు
         </div>
 
         <div className="lang" onClick={() => handleLanguageClick('hi')}>
-          <div className="sound"></div>
+          <div
+            className="sound"
+            onClick={(e) => handleSpeak(e, 'हिन्दी', 'hi-IN')}
+          ></div>
           हिन्दी
         </div>
 
         <div className="lang" onClick={() => handleLanguageClick('ta')}>
-          <div className="sound"></div>
+          <div
+            className="sound"
+            onClick={(e) => handleSpeak(e, 'தமிழ்', 'ta-IN')}
+          ></div>
           தமிழ்
         </div>
       </div>
