4ae7e5edbf830c658e4969f6cd679b2cd40a6366
kutikuppala chetan srinivas <kutikuppala.chetan2023@vitstudent.ac.in>
2025-07-28 22:23:34 +0530

    Update AIAssistantPage.jsx

--- src/AIAssistantPage.jsx
+++ src/AIAssistantPage.jsx
@@ -3,8 +3,6 @@ import AgriExpert from './AgriExpert';
 import CropDoctor from './CropDoctor';
 
 export default function AIAssistantPage() {
-    // Note: The CSS classes for .card, .btn-primary, .loader, etc.
-    // should be in your global CSS file (e.g., index.css).
     return (
         <div className="container mx-auto p-4 md:p-8 max-w-6xl">
             <header className="text-center mb-12">
