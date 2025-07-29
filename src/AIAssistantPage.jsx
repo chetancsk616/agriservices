678b36be6f180e2d6882abcbb22846687c9a3f63
kutikuppala chetan srinivas <kutikuppala.chetan2023@vitstudent.ac.in>
2025-07-29 22:33:53 +0530

    Update AIAssistantPage.jsx

--- src/AIAssistantPage.jsx
+++ src/AIAssistantPage.jsx
@@ -9,7 +9,7 @@ const API_BASE_URL = 'https://agriservices-production-e14a.up.railway.app/';
 
 // --- SVG Icons ---
 const UserIcon = () => (
-    <svg className="w-8 h-8 text-white bg-green-600 rounded-full p-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
+    <svg className="w-0.1 h-0.1 text-white bg-green-600 rounded-full p-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
 );
 
 const AiIcon = () => (
